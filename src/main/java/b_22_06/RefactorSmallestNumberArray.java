a5fe5148600ff0d3172dbd0c99378ce8d5176143
Pacurar Cosmin <nadal19842002@yahoo.com>
2019-06-21 22:35:09 +0300

    Salut, Ionut! Am incercat sa fac exercitiile 3 si 4(aplicatia bancara si biblioteca de carti), sper ca am reusit sa fac ceva. La punctul 2 recunosc ca am facut pe fuga refactorizarea in metode. Sa vad cum atasez rezultatele exercitiilor de pe hackerrank ca nu imi merge printscreen-ul.

---
+++ src/main/java/b_22_06/RefactorSmallestNumberArray.java
@@ -0,0 +1,25 @@
+package b_22_06;
+
+public class RefactorSmallestNumberArray {
+    public static void main(String[] args) {
+        int [] array = {1,4,7};
+        System.out.print(smallest(array));
+    }
+    public static int smallest (int[] a){
+         a = {10,20,30};
+        int min = a[0];
+        for(int i=0;i<=a.length-1;i++){
+            if (a[i] < min)
+                min = a[i];}
+        for (int e: a) {
+            if (e < min)
+                System.out.println("Elementul cu valoarea minima este" + " " + e);
+            else
+                continue;
+        }
+        return min;
+    }
+
+
+
+}
---
+++ src/main/java/b_22_06/libraryCatalog/Testare.java
@@ -0,0 +1,21 @@
+package b_22_06.libraryCatalog;
+
+public class Testare {
+    public static void main(String[] args) {
+        // TODO Auto-generated metho
+        Nuvele nuvela = new Nuvele("Neamul Soimarestilor");
+        nuvela.adaugaCarti("Ion");
+        nuvela.adaugaCarti("Cel mai iubit dintre pamanteni");
+        nuvela.adaugaCarti("Halkidiki");
+
+        AlbumeArta albume = new AlbumeArta("Picasso");
+        albume.adaugaCarti("Rembrandt");
+        albume.adaugaCarti("Hagi");
+        albume.adaugaCarti("Caracuda");
+
+        albume.stergeCarti("Picasso");
+
+
+
+    }
+}
