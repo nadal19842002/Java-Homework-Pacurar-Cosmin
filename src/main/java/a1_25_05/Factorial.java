7e255da1c0a3cd7eba8fbb12f80472872e7f71d4
Pacurar Cosmin <nadal19842002@yahoo.com>
2019-06-29 00:26:53 +0300

    Salut, Ionut!
    Intamplator, am vazut ca tema nr1 nu a ajuns nu a ajuns la remote repository.
    Sper ca acum sa ajunga :).
    
    Mc
    
    Cosmin

---
+++ src/main/java/a1_25_05/Factorial.java
@@ -0,0 +1,26 @@
+package a1_25_05;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        /*pasul 1. creem un obiect din clasa Scanner(tastatura) pentru a permite utilizatorului
+        sa introduca un numar intreg*/
+        Scanner scanner = new Scanner(System.in);
+        //pasul 2. solicitam utilizatorului sa introducaa un numar intreg intre 1 si 300
+        System.out.println("Introdu un numar intreg intre 1 si 10: ");
+        //pasul 3. atribuim referinta obiectului creat(tastatura) catre variabila a
+        int a = scanner.nextInt();
+        /*pasul 4. declaram si initializam o variabila in care vom retine rezultatul parcurgerii temporare a
+        factorialului, precum si o variabila in care retinem valoarea factorialului*/
+        int d,factorial=1;
+        //pasul 6. Cu o bucla imbricata for calculam factorialul numarului introdus de utilizator
+        for(int i=1;i<=1;i++){
+            for (int j=1;j<=a;j++){
+                 d = i*j;
+                 factorial=factorial*d;
+            }
+        }
+        //pasul 5. afisam rezultatul
+        System.out.print(factorial);
+    }
+}
---
+++ src/main/java/a1_25_05/MaxDigitNumber.java
@@ -0,0 +1,23 @@
+package a1_25_05;
+import java.util.Scanner;
+
+public class MaxDigitNumber {
+    public static void main(String[] args) {
+        /*pasul 1. creem un obiect din clasa Scanner (tastatura) pt a solicita utilizatorului sa introduca
+        un numar de la 1 la 127*/
+        Scanner input = new Scanner(System.in);
+        //pasul 2. Solicitam utilizatorului sa introduca un numar intreg de la 1 la 127
+        System.out.println("Introdu de la tastatura un numar intreg : ");
+        int a = input.nextInt();
+        /*pasul 3. definim cifra 1 ca fiind digit-ul maxim.cu o bucla while identificam fiecare cifra a numarului,iar ulterior o comparam pe fiecare cu cifra maxima*/
+        int maxDigit=1;
+        while(a!=0){
+            int b=a%10;
+            if(b>maxDigit){
+                maxDigit=b;}
+            a=a/10;
+        }
+        //pasul 4. afisam rezultatul
+        System.out.println("Valoarea maxima a cifrei numarului propus este"+" "+maxDigit);
+    }
+}
